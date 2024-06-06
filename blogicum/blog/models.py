e93fa48d2b1b863c277c363e2c5bcc94e51ac4b0
Sofia Pogosian <sonyapgsn@gmail.com>
2024-06-06 09:07:42 +0300

    fourth_iteration

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class BaseModel(models.Model):
+class BaseBlogModel(models.Model):
     is_published = models.BooleanField(
         'Опубликовано',
         default=True,
@@ -18,57 +18,69 @@ class BaseModel(models.Model):
         ordering = ('created_at', )
 
 
-class Location(BaseModel):
+class Location(BaseBlogModel):
     name = models.CharField(
         'Название места', max_length=settings.MAX_FIELD_LENGTH)
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
+        ordering = BaseBlogModel.Meta.ordering
 
     def __str__(self) -> str:
         return self.name[:settings.REPRESENTATION_LENGTH]
 
 
-class Category(BaseModel):
+class Category(BaseBlogModel):
     title = models.CharField('Заголовок', max_length=settings.MAX_FIELD_LENGTH)
     description = models.TextField('Описание')
-    slug = models.SlugField('Идентификатор',
-                            max_length=settings.MAX_FIELD_LENGTH,
-                            unique=True,
-                            help_text='Идентификатор страницы для URL; '
-                            'разрешены символы латиницы, цифры, '
-                            'дефис и подчёркивание.')
+    slug = models.SlugField(
+        'Идентификатор',
+        max_length=settings.MAX_FIELD_LENGTH,
+        unique=True,
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, '
+            'дефис и подчёркивание.'
+        )
+    )
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
+        ordering = BaseBlogModel.Meta.ordering
 
     def __str__(self) -> str:
         return self.title[:settings.REPRESENTATION_LENGTH]
 
 
-class Post(BaseModel):
+class Post(BaseBlogModel):
     title = models.CharField('Заголовок', max_length=settings.MAX_FIELD_LENGTH)
     text = models.TextField('Текст')
-    pub_date = models.DateTimeField('Дата и время публикации',
-                                    help_text='Если установить дату и время '
-                                    'в будущем — можно '
-                                    'делать отложенные публикации.')
+    pub_date = models.DateTimeField(
+        'Дата и время публикации',
+        help_text=(
+            'Если установить дату и время в будущем — можно '
+            'делать отложенные публикации.'
+        )
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name='Автор публикации')
+        User, on_delete=models.CASCADE, verbose_name='Автор публикации'
+    )
     location = models.ForeignKey(
         Location, on_delete=models.SET_NULL, null=True, blank=True,
-        verbose_name='Местоположение')
+        verbose_name='Местоположение'
+    )
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True,
-        verbose_name='Категория')
+        verbose_name='Категория'
+    )
 
     class Meta:
         default_related_name = 'posts'
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ('-pub_date', )
+        ordering = ('-pub_date', BaseBlogModel.Meta.ordering[0])
 
     def __str__(self):
         return self.title[:settings.REPRESENTATION_LENGTH]
